37d9576939069dbc566793f2676608051e3a8541
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-08-02 16:13:06 +0330

    Update todo_filter_state.dart

--- todo/lib/bloc/todo_filter/todo_filter_state.dart
+++ todo/lib/bloc/todo_filter/todo_filter_state.dart
@@ -1,6 +1,26 @@
 part of 'todo_filter_bloc.dart';
 
-@immutable
-abstract class TodoFilterState {}
+abstract class TodoFilterState extends Equatable {}
 
-class TodoFilterInitial extends TodoFilterState {}
+class TodoFilterLoading extends TodoFilterState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class TodoFilterLoaded extends TodoFilterState {
+  final List<TodoModel> filtredTodos;
+  final TodoFilter todoFilter;
+
+  TodoFilterLoaded({
+    required this.filtredTodos,
+    this.todoFilter = TodoFilter.all,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        filtredTodos,
+        todoFilter,
+      ];
+}
