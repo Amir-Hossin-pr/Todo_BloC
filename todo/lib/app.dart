0481b31cab344da309c3bb0bcf4434e4955cb94f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-08-02 14:56:22 +0330

    Update app.dart

--- todo/lib/app.dart
+++ todo/lib/app.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/bloc/bloc/todo_bloc.dart';
+import 'package:todo/models/todo.dart';
+import 'package:todo/pages/home/index.dart';
 
 class TodoApp extends StatelessWidget {
   const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => TodoBloc()
+            ..add(
+              LoadTodos(
+                todos: TodoModel.todos,
+              ),
+            ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: HomeScreen(),
+      ),
+    );
   }
 }
