58e19ab205aaf1bc5907e3f7a33d7a3f5dae9acd
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-08-02 16:13:01 +0330

    Update todo_filter_bloc.dart

--- todo/lib/bloc/todo_filter/todo_filter_bloc.dart
+++ todo/lib/bloc/todo_filter/todo_filter_bloc.dart
@@ -1,13 +1,69 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:todo/bloc/todo/todo_bloc.dart';
+import 'package:todo/models/todo.dart';
+
+import '../../models/todo_filter.dart';
 
 part 'todo_filter_event.dart';
 part 'todo_filter_state.dart';
 
 class TodoFilterBloc extends Bloc<TodoFilterEvent, TodoFilterState> {
-  TodoFilterBloc() : super(TodoFilterInitial()) {
-    on<TodoFilterEvent>((event, emit) {
-      // TODO: implement event handler
+  final TodoBloc _todoBloc;
+  late StreamSubscription _todoSubscription;
+
+  TodoFilterBloc({required TodoBloc todoBloc})
+      : _todoBloc = todoBloc,
+        super(TodoFilterLoading()) {
+    on<UpdateFilter>(_onUpdateFilter);
+    on<UpdateTodos>(_onUpdateTodos);
+
+    _todoSubscription = _todoBloc.stream.listen((state) {
+      add(
+        UpdateTodos(),
+      );
     });
+  }
+
+  void _onUpdateFilter(UpdateFilter event, Emitter<TodoFilterState> emit) {
+    if (state is TodoFilterLoading) {
+      add(
+        UpdateTodos(todoFilter: TodoFilter.pending),
+      );
+    }
+
+    if (state is TodoFilterLoaded) {
+      final state = this.state as TodoFilterLoaded;
+
+      add(
+        UpdateTodos(todoFilter: state.todoFilter),
+      );
+    }
+  }
+
+  void _onUpdateTodos(UpdateTodos event, Emitter<TodoFilterState> emit) {
+    final state = _todoBloc.state;
+    if (state is TodoLoaded) {
+      List<TodoModel> todos = state.todos.where((todo) {
+        switch (event.todoFilter) {
+          case TodoFilter.all:
+            return true;
+          case TodoFilter.cancled:
+            return todo.isCancled;
+          case TodoFilter.completed:
+            return todo.isCompelete;
+          case TodoFilter.pending:
+            return !(todo.isCompelete && todo.isCancled);
+          default:
+            return true;
+        }
+      }).toList();
+
+      emit(
+        TodoFilterLoaded(filtredTodos: todos),
+      );
+    }
   }
 }
