5fdd227765fa15b6ac2a4fbddde23d7e09ec0cae
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-08-02 14:33:28 +0330

    create bloc events

--- todo/lib/bloc/bloc/todo_bloc.dart
+++ todo/lib/bloc/bloc/todo_bloc.dart
@@ -1,13 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:todo/models/todo.dart';
 
 part 'todo_event.dart';
 part 'todo_state.dart';
 
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
-  TodoBloc() : super(TodoInitial()) {
-    on<TodoEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  TodoBloc() : super(TodoLoading()) {
+    on<LoadTodos>(_onLoadTodos);
+    on<AddTodo>(_onAddTodo);
+    on<UpdateTodo>(_onUpdateTodo);
+    on<DeleteTodo>(_onDeleteTodo);
   }
+
+  void _onLoadTodos(LoadTodos event, Emitter<TodoState> emit) {}
+
+  void _onAddTodo(AddTodo event, Emitter<TodoState> emit) {}
+
+  void _onUpdateTodo(UpdateTodo event, Emitter<TodoState> emit) {}
+
+  void _onDeleteTodo(DeleteTodo event, Emitter<TodoState> emit) {}
 }
--- todo/lib/bloc/bloc/todo_event.dart
+++ todo/lib/bloc/bloc/todo_event.dart
@@ -1,4 +1,50 @@
 part of 'todo_bloc.dart';
 
 @immutable
-abstract class TodoEvent {}
+abstract class TodoEvent extends Equatable {}
+
+class LoadTodos extends TodoEvent {
+  final List<TodoModel> todos;
+
+  LoadTodos({this.todos = const <TodoModel>[]});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [todos];
+}
+
+class AddTodo extends TodoEvent {
+  final TodoModel todo;
+
+  AddTodo({
+    required this.todo,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [todo];
+}
+
+class UpdateTodo extends TodoEvent {
+  final TodoModel todo;
+
+  UpdateTodo({
+    required this.todo,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [todo];
+}
+
+class DeleteTodo extends TodoEvent {
+  final TodoModel todo;
+
+  DeleteTodo({
+    required this.todo,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [todo];
+}
