ddd2e3fdde204a58c2e8bb55e0f15a6dc60c5571
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-08-02 16:26:29 +0330

    Update index.dart

--- todo/lib/pages/home/index.dart
+++ todo/lib/pages/home/index.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/bloc/todo_filter/todo_filter_bloc.dart';
+import 'package:todo/models/todo_filter.dart';
 import 'package:todo/pages/list/todos_list.dart';
 import 'package:todo/pages/upsert/upsert_todo.dart';
 
@@ -7,17 +10,56 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todo App'),
-        elevation: 4,
-        backgroundColor: Colors.lightBlue,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Todo App'),
+          elevation: 4,
+          backgroundColor: Colors.lightBlue,
+          bottom:
+              TabBar(onTap: (index) => _changeTab(context, index), tabs: const [
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.more_horiz),
+                  Text('All'),
+                ],
+              ),
+            ),
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.pending_actions),
+                  Text('Pending'),
+                ],
+              ),
+            ),
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.check),
+                  Text('Completed'),
+                ],
+              ),
+            )
+          ]),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _addTodo(context),
+          child: const Icon(Icons.add),
+        ),
+        body: const TabBarView(
+          children: [
+            TodosListView(),
+            TodosListView(),
+            TodosListView(),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _addTodo(context),
-        child: const Icon(Icons.add),
-      ),
-      body: TodosListView(),
     );
   }
 
@@ -27,5 +69,26 @@ class HomeScreen extends StatelessWidget {
         MaterialPageRoute(
           builder: (context) => UpsertTodoScreen(),
         ));
+  }
+
+  void _changeTab(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        BlocProvider.of<TodoFilterBloc>(context).add(UpdateTodos(
+          todoFilter: TodoFilter.all,
+        ));
+        break;
+      case 1:
+        BlocProvider.of<TodoFilterBloc>(context).add(UpdateTodos(
+          todoFilter: TodoFilter.pending,
+        ));
+        break;
+      case 2:
+        BlocProvider.of<TodoFilterBloc>(context).add(UpdateTodos(
+          todoFilter: TodoFilter.completed,
+        ));
+        break;
+      default:
+    }
   }
 }
