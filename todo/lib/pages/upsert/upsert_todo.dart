c1a9297f05c41fdefa1cc95e3661cebcf8885ec9
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-08-02 15:29:12 +0330

    submit todo

--- todo/lib/pages/upsert/upsert_todo.dart
+++ todo/lib/pages/upsert/upsert_todo.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/bloc/bloc/todo_bloc.dart';
+import 'package:todo/models/todo.dart';
 import 'package:todo/pages/widget/button.dart';
 import 'package:todo/pages/widget/input.dart';
 
@@ -6,8 +9,12 @@ class UpsertTodoScreen extends StatelessWidget {
   UpsertTodoScreen({super.key});
 
   //? Controllers
+  final id = TextEditingController();
   final title = TextEditingController();
   final description = TextEditingController();
+
+  //? Key
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -16,36 +23,79 @@ class UpsertTodoScreen extends StatelessWidget {
         title: const Text('Add or Update Todo'),
         backgroundColor: Colors.lightBlue,
       ),
-      body: Container(
-        margin: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            InputOutline(
-                clearable: true,
-                controller: title,
-                hint: 'Title',
-                lable: 'Title',
-                maxLines: 1,
-                isRequired: true),
-            const SizedBox(height: 15),
-            InputOutline(
-                clearable: true,
-                controller: description,
-                hint: 'Description',
-                lable: 'Description',
-                maxLines: 4,
-                isRequired: true),
-            const SizedBox(height: 15),
-            Button(
-              onPressed: () {},
-              color: Colors.lightBlue,
-              lable: 'Submit',
-              icon: const Icon(Icons.check),
-              borderRadius: 15,
-            )
-          ],
+      body: BlocListener<TodoBloc, TodoState>(
+        listener: (context, state) {
+          if (state is TodoLoaded) {
+            id.text = '';
+            title.text = '';
+            description.text = '';
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Success fully add todo'),
+              ),
+            );
+            Navigator.pop(context);
+          }
+        },
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          child: Form(
+              key: key,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                children: [
+                  InputOutline(
+                      clearable: true,
+                      controller: id,
+                      hint: 'Id',
+                      lable: 'Id',
+                      maxLines: 1,
+                      isRequired: true,
+                      errorText: 'Please input some id'),
+                  const SizedBox(height: 15),
+                  InputOutline(
+                      clearable: true,
+                      controller: title,
+                      hint: 'Title',
+                      lable: 'Title',
+                      maxLines: 1,
+                      isRequired: true,
+                      errorText: 'Please input some title'),
+                  const SizedBox(height: 15),
+                  InputOutline(
+                      clearable: true,
+                      controller: description,
+                      hint: 'Description',
+                      lable: 'Description',
+                      maxLines: 4,
+                      isRequired: true,
+                      errorText: 'Please input some description'),
+                  const SizedBox(height: 15),
+                  Button(
+                    onPressed: () => _submitTodo(context),
+                    color: Colors.lightBlue,
+                    lable: 'Submit',
+                    icon: const Icon(Icons.check),
+                    borderRadius: 15,
+                  )
+                ],
+              )),
         ),
       ),
     );
+  }
+
+  void _submitTodo(BuildContext contex) {
+    if (key.currentState!.validate()) {
+      contex.read<TodoBloc>().add(
+            AddTodo(
+              todo: TodoModel(
+                title: title.text,
+                description: description.text,
+                id: id.text,
+              ),
+            ),
+          );
+    }
   }
 }
