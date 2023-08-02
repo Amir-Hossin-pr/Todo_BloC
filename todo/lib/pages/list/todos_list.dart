d2d4a7056dbf71187b0c4f58c9c9b87f6a4446b8
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-08-02 14:56:18 +0330

    create todos list pagee

---
+++ todo/lib/pages/list/todos_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/bloc/bloc/todo_bloc.dart';
+
+class TodosListView extends StatelessWidget {
+  const TodosListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TodoBloc, TodoState>(builder: (context, state) {
+      if (state is TodoLoading) {
+        return const Center(child: CircularProgressIndicator());
+      }
+      if (state is TodoLoaded) {
+        return Container(
+          margin: const EdgeInsets.all(10),
+          child: ListView.builder(
+            itemCount: state.todos.length,
+            itemBuilder: (context, index) {
+              final todo = state.todos[index];
+              return ListTile(
+                title: Text(todo.title),
+                leading: (todo.isCompelete ?? false)
+                    ? const Icon(Icons.check_circle_outline)
+                    : const Icon(Icons.circle_outlined),
+              );
+            },
+          ),
+        );
+      } else {
+        return const Center(
+          child: Text('Somthing went wrong!'),
+        );
+      }
+    });
+  }
+}
